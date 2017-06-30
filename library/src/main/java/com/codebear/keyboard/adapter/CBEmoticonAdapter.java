0afb82bc721c52068bffda36cb1baadddb7e8dbf
CodeBear <249900679@qq.com>
2017-06-30 17:50:08 +0800

    [修改] 解决glide加载git为bitmap是透明背景变为黑色的bug

--- library/src/main/java/com/codebear/keyboard/adapter/CBEmoticonAdapter.java
+++ library/src/main/java/com/codebear/keyboard/adapter/CBEmoticonAdapter.java
@@ -1,6 +1,7 @@
 package com.codebear.keyboard.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 import com.codebear.keyboard.R;
 import com.codebear.keyboard.data.EmoticonAdapterBean;
 import com.codebear.keyboard.data.EmoticonsBean;
@@ -86,8 +89,16 @@ public class CBEmoticonAdapter extends BaseAdapter {
 
     private void showData(final ViewHolder viewHolder, final int position) {
         if (mData.getmData().get(position).getIconUri() != null) {
-            Glide.with(mContext).load(mData.getmData().get(position).getIconUri()).asBitmap().dontAnimate().into
-                    (viewHolder.ivIcon);
+            Glide.with(mContext).load(mData.getmData().get(position).getIconUri()).asBitmap().dontAnimate().into(new SimpleTarget<Bitmap>() {
+                @Override
+                public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                    if("gif".equals(mData.getmData().get(position).getIconType())) {
+                        resource = eraseColor(resource, -1);
+                        resource = eraseColor(resource, -16777216);
+                    }
+                    viewHolder.ivIcon.setImageBitmap(resource);
+                }
+            });
 
             viewHolder.rootView.setOnTouchListener(new View.OnTouchListener() {
                 @Override
@@ -117,6 +128,23 @@ public class CBEmoticonAdapter extends BaseAdapter {
             viewHolder.tvTitle.setVisibility(View.VISIBLE);
             viewHolder.tvTitle.setText(mData.getmData().get(position).getName());
         }
+    }
+
+    //BitmapUtil中擦除Bitmap像素的方法
+    public Bitmap eraseColor(Bitmap src, int color) {
+        int width = src.getWidth();
+        int height = src.getHeight();
+        Bitmap b = src.copy(Bitmap.Config.ARGB_8888, true);
+        b.setHasAlpha(true);
+        int[] pixels = new int[width * height];
+        src.getPixels(pixels, 0, width, 0, 0, width, height);
+        for (int i = 0; i < width * height; i++) {
+            if (pixels[i] == color) {
+                pixels[i] = 0;
+            }
+        }
+        b.setPixels(pixels, 0, width, 0, 0, width, height);
+        return b;
     }
 
     private void updateUI(ViewHolder viewHolder, ViewGroup parent) {
